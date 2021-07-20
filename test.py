dabaffef5f18747962f3b961a918d727c3f3a2c3
吴彰涛 <1182517419@qq.com>
2021-07-20 17:35:55 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/10 10:34
+# @Author  : ztwu4
+# @Email   : ztwu4@iflytek.com
+# @File    : test.py
+# @Software: PyCharm
+
+a = list([1,2,5,2,6,4,3])
+a = sorted(a,reverse=True)
+print(a)
